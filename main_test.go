160d63701fb2dedd9d1012539b50b1d0be6067cf
Ryan Uber <ru@ryanuber.com>
2015-04-09 17:19:19 -0700

    Main tests

---
+++ main_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestMain_fails(t *testing.T) {
+	// Returns 1 on bad args
+	if code := realMain([]string{}); code != 1 {
+		t.Fatalf("bad: %d", code)
+	}
+	if code := realMain([]string{"1", "2", "3"}); code != 1 {
+		t.Fatalf("bad: %d", code)
+	}
+
+	// Returns 1 on bad data-dir
+	if code := realMain([]string{"consul-migrate", "/unicorns"}); code != 1 {
+		t.Fatalf("bad: %d", code)
+	}
+}
+
+func TestMain_help(t *testing.T) {
+	fh, err := ioutil.TempFile("", "consul-migrate")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	defer os.Remove(fh.Name())
+
+	stdoutOrig := *os.Stdout
+	os.Stdout = fh
+	defer func() {
+		os.Stdout = &stdoutOrig
+	}()
+
+	if code := realMain([]string{"consul-migrate", "-h"}); code != 0 {
+		t.Fatalf("bad: %d", code)
+	}
+
+	if _, err := fh.Seek(0, 0); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	out, err := ioutil.ReadAll(fh)
+	if !strings.HasPrefix(string(out), "Usage:") {
+		t.Fatalf("bad: %s", string(out))
+	}
+}
